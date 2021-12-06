f3254964b7e2d083a300ee033d2ae68d187a74e8
Dejan <deki90to@gmail.com>
2021-12-07 00:14:51 +0100

    Update settings.py

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -141,6 +141,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'deki90to@gmail.com'
-EMAIL_HOST_PASSWORD = 'dejan90to'
+EMAIL_HOST_PASSWORD = 'password'
 
 # MEDIA_URL = '/media/'
